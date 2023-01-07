c792850aaffaf5d29b6bad82e76a05411f7328bb
mikekang47 <gyeong0308@gmail.com>
2023-01-07 17:32:23 +0900

    Feat: add createUser api

---
+++ user/user_controller.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, HTTPException
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+from starlette import status
+
+import jwt_config
+from database import get_db
+from user import user_application, user_schema
+
+router = APIRouter(
+    prefix="/users",
+)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=user_schema.UserResponse)
+def user_create(_user_create: user_schema.UserCreate, db: Session = Depends(get_db)):
+    user = user_application.get_existing_user(db, user_create=_user_create)
+    if user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="이미 존재하는 사용자입니다.")
+    user = user_application.create_user(db=db, user_create=_user_create)
+    return user_schema.UserResponse(nick_name=user.nick_name, email=user.email)
+
+
+def get_current_user(token: str = Depends(oauth2_scheme),
+                     db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, jwt_config.getSecretKey(), algorithms=[jwt_config.getAlgorithm()])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    else:
+        user = user_application.get_user(db, username=username)
+        if user is None:
+            raise credentials_exception
+        return user
