83e4aabaf6a6eca5386aa59e6852a6edfc45b286
mikekang47 <gyeong0308@gmail.com>
2023-01-07 21:51:55 +0900

    Feat: create logout api

--- session/session_controller.py
+++ session/session_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Header
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -7,7 +7,6 @@ from starlette import status
 from database import get_db
 from session import session_application, session_schema
 from user import user_application
-from user.user_application import pwd_context
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/session/login")
 
@@ -17,17 +16,16 @@ router = APIRouter(
 
 
 @router.post("/login", response_model=session_schema.Token)
-def login_for_access_token(login_data: OAuth2PasswordRequestForm = Depends(),
-                           db: Session = Depends(get_db)):
+def login(login_data: OAuth2PasswordRequestForm = Depends(),
+          db: Session = Depends(get_db)):
     # check user and password
-    # TODO
-    # verfiy랑 create 책임 분리
-    user = user_application.get_user(db, login_data.username)
-    if not user or not pwd_context.verify(login_data.password, user.password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    user = user_application.get_user(db, email=login_data.username)
+    session_application.verfiy(user, login_data.password)
 
-    return session_application.create_session(user.nick_name)
+    return session_application.create_session(db, user.email)
+
+
+@router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
+def logout(token: str = Depends(oauth2_scheme),
+           db: Session = Depends(get_db)):
+    session_application.delete_session(db, token)
--- session/session_schema.py
+++ session/session_schema.py
@@ -3,11 +3,29 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class Token(BaseModel):
+class RefreshToken(BaseModel):
+    refresh_token: str
+    token_type: str
+    expire_date: datetime
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class AccessToken(BaseModel):
     access_token: str
     token_type: str
-    nick_name: str
-    expire_time: datetime
+    email: str
+    expire_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: AccessToken
+    refresh_token: RefreshToken
 
     class Config:
         orm_mode = True
