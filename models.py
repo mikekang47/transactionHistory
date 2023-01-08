35aa8fcc065154197f78f8b75082e25413f1cddd
mikekang47 <gyeong0308@gmail.com>
2023-01-08 14:47:30 +0900

    Feat: extract history model from models.py

--- models.py
+++ models.py
@@ -1,7 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
-from sqlalchemy.orm import relationship
-
-from database import Base
+from sqlalchemy import Column, Integer, DateTime, func
 
 
 class BaseMixin:
@@ -11,13 +8,3 @@ class BaseMixin:
                         nullable=False,
                         default=func.utc_timestamp(),
                         onupdate=func.utc_timestamp())
-
-
-class Histroy(Base, BaseMixin):
-    __tablename__ = 'histories'
-    user_id = Column(Integer, ForeignKey('users.id'))
-    post_id = Column(Integer, ForeignKey('posts.id'))
-    discription = Column(String(255))
-    money = Column(Integer, nullable=False)
-    real_transaction_date = Column(DateTime, nullable=False)
-    current_user = relationship('User', backref='histories')
