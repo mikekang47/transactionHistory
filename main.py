702babce6be12f5a4a0eab8cc7866aac3fb830d4
mikekang47 <gyeong0308@gmail.com>
2023-01-08 14:26:30 +0900

    Feat: add transaciton api router

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
 from session import session_controller
+from transaction import transaction_controller
 from user import user_controller
 
 app = FastAPI()
 
 app.include_router(user_controller.router)
 app.include_router(session_controller.router)
-
+app.include_router(transaction_controller.router)
 
 @app.get("/")
 async def root():
