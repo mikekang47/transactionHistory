968fdf36c64e96ee4209359d8439aa8548f33d35
mikekang47 <gyeong0308@gmail.com>
2023-01-08 23:57:42 +0900

    Feat: create property for schema

--- session/session_schema.py
+++ session/session_schema.py
@@ -4,20 +4,28 @@ from pydantic import BaseModel
 
 
 class RefreshToken(BaseModel):
-    refresh_token: str
+    _token: str
     token_type: str
     expire_date: datetime
     email: str
+
+    @property
+    def get_token(self):
+        return self._token
 
     class Config:
         orm_mode = True
 
 
 class AccessToken(BaseModel):
-    access_token: str
+    _token: str
     token_type: str
     email: str
     expire_date: datetime
+
+    @property
+    def get_token(self):
+        return self._token
 
     class Config:
         orm_mode = True
@@ -26,6 +34,14 @@ class AccessToken(BaseModel):
 class Token(BaseModel):
     access_token: AccessToken
     refresh_token: RefreshToken
+
+    @property
+    def get_access_token(self):
+        return self.access_token.get_token
+
+    @property
+    def get_refresh_token(self):
+        return self.refresh_token.get_token
 
     class Config:
         orm_mode = True
