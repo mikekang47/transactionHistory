cb12b649564db1fc68bb95ea71d094f757b7d867
mikekang47 <gyeong0308@gmail.com>
2023-01-08 16:29:37 +0900

    Feat: change delete history way

--- transaction/transaction_model.py
+++ transaction/transaction_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import relationship, backref
 
 from database import Base
@@ -11,8 +11,12 @@ class History(Base, BaseMixin):
     user_id = Column(Integer, ForeignKey('users.id'))
     detail = Column(String(255), nullable=True, default='')
     money = Column(Integer, nullable=False, default=0)
+    is_deleted = Column(Boolean, nullable=False, default=False)
     user = relationship('User', backref=backref('histories', order_by=id))
 
     def updateHistory(self, detail, money):
         self.detail = detail
         self.money = money
+
+    def deleteHistory(self):
+        self.is_deleted = True
