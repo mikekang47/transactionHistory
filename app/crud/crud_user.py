6461f0f316ac9c592c4932e84f905ff534851bf9
mikekang47 <gyeong0308@gmail.com>
2023-01-09 15:21:59 +0900

    Refactor: refactoring user crud exceptions

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -1,10 +1,11 @@
+from fastapi import HTTPException
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from starlette import status
 
 from app.crud.base import CRUDBase
 from app.models.user import User
 from app.schemas import UserCreate
-from error import user_exception
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -13,7 +14,8 @@ class CRUDUser(CRUDBase[User, UserCreate]):
     def create_user(self, db: Session, *, user_create: UserCreate):
         exists_user = self.__get_existing_user(db, user_create=user_create)
         if exists_user is not None:
-            raise user_exception.UserExistsException()
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail="User already exists")
 
         source = User(nick_name=user_create.nick_name,
                       email=user_create.email,
@@ -32,7 +34,8 @@ class CRUDUser(CRUDBase[User, UserCreate]):
     def get_user_by_email(self, db: Session, *, email: str) -> User:
         source = db.query(User).filter(User.email == email).first()
         if source is None:
-            raise user_exception.UserNotFoundException()
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="User not found")
         return source
 
 
